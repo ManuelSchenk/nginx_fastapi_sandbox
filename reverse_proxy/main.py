1bb079d017c8342761f4cd69e4e576327be635c0
Schenk Manuel (XC-AS/EPA3) <Manuel.Schenk@de.bosch.com>
2025-03-24 07:07:14 +0100

    add basic http auth to nginx

--- reverse_proxy/main.py
+++ reverse_proxy/main.py
@@ -5,20 +5,19 @@ import uvicorn
 from utils.logging import ic
 
 app = FastAPI()
-container_name = os.popen("hostname").read().strip()
-instance_name = os.environ.get("INSTANCE_NAME", "Unknown Instance")
+host_name = os.popen("hostname").read().strip()
 
 @app.get("/health_check")
 def health_check():
-    response = f"API is healthy on container {instance_name}:{container_name}"
+    response = f"API is healthy on host (container) {host_name}"
     ic(response)
     return {"message": response}
 
 @app.post("/long_running_process")
 def long_runner():
-    ic("Long running Process started. The Server is going to be blocked for 60 seconds...")
-    time.sleep(60)  # Block for 60 seconds
-    return {"message": f"API on conatiner {instance_name}:{container_name} was blocked for 60 seconds."}
+    ic("Long running Process started. The Server is going to be blocked for 5 seconds...")
+    time.sleep(5)  # Block for 60 seconds
+    return {"message": f"API on host (container) {host_name} was blocked for 60 seconds."}
 
 if __name__ == "__main__":
     ic("Starting FastAPI server on port 3003...")
--- reverse_proxy/utils/logging.py
+++ reverse_proxy/utils/logging.py
@@ -8,9 +8,9 @@ def custom_ic_output(arg):
     frame = inspect.currentframe().f_back.f_back
     line_number = frame.f_lineno
     
-    container_name = os.popen("hostname").read().strip()
+    host_name = os.popen("hostname").read().strip()
     
-    output_line = f"{datetime.now().strftime('%Y%m%d_%H:%M:%S')} | container: {container_name} | Line: {line_number} | {arg[3:].strip()}\n"
+    output_line = f"{datetime.now().strftime('%Y%m%d_%H:%M:%S')} | container: {host_name} | Line: {line_number} | {arg[3:].strip()}\n"
     with open('ic.log', 'a') as f:
         f.write(output_line)
     print(output_line, flush=True)
